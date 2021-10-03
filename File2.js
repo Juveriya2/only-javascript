2f02d35f91ef51d4b5b9096ada07cd0902437d75
Juveriya2 <gmsjuveriya@gmail.com>
2021-10-03 17:32:20 +0530

    homework

---
+++ File2.js
@@ -0,0 +1,52 @@
+james = 12;
+james = 12;
+smith = 49;
+andy = 23;
+
+if {
+  (james == andy)
+  james12 chocolate= andy4 chocolates- smith2 chocolates;
+}
+else {
+  smith49 = andy7 - 10 james;
+}
+console.log(james);
+console.log(smith);
+console.log(andy); = 49;
+andy = 23;
+
+console.log(james);
+console.log(smith);
+console.log(andy);
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+
+
+
+
