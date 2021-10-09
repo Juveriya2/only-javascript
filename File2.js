326fd0ae89220a975ab7d926d13ac65a747a1411
Juveriya2 <gmsjuveriya@gmail.com>
2021-10-09 18:07:54 +0530

    making changes in homework

--- File2.js
+++ File2.js
@@ -3,13 +3,22 @@ james = 12;
 smith = 49;
 andy = 23;
 
+
 if {
   (james == andy)
-  james12 chocolate= andy4 chocolates- smith2 chocolates;
+   james give 4 chocolates to andy
+ andy=andy+4;
+ james=james-4;
+ console.log(andy,james);
 }
+
 else {
-  smith49 = andy7 - 10 james;
+  smith give 7 chocolate to andy and 10 chocolates to james
+  smith=smith-7
+  andy=andy+7
+  console.log(smith,andy)
 }
+
 console.log(james);
 console.log(smith);
 console.log(andy); = 49;
